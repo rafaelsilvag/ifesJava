62a4af22e738af9c7f8007efa947e8dcdf88a017
Rafael Silva Guimarães <rafaelg@ifes.edu.br>
2015-09-15 15:11:30 -0300

    Principal.java
    - Implementação do restante das opções.

--- SI-2015-2/ExemploBanco/views/Principal.java
+++ SI-2015-2/ExemploBanco/views/Principal.java
@@ -89,12 +89,117 @@ public class Principal {
                         JOptionPane.showMessageDialog(null, res);
                         break;
                     case 3:
+                        String idConta;
+                        String vTrans;
+                        ContaCorrente contaOrigem = null;
+                        ContaCorrente contaDestino = null;
+                        
+                        idConta = JOptionPane.showInputDialog( "Informe a C/C Origem");
+                        
+                        for(ContaCorrente cc: listaContas){
+                            if(cc.getId() == Integer.parseInt(idConta)){
+                                contaOrigem = cc;
+                                break;
+                            }
+                        }
+                        
+                        idConta = JOptionPane.showInputDialog("Informe a C/C Destino");
+                        
+                        for(ContaCorrente cc: listaContas){
+                            if(cc.getId() == Integer.parseInt(idConta)){
+                                contaDestino = cc;
+                                break;
+                            }
+                        }
+                        
+                        vTrans = JOptionPane.showInputDialog("Informe o valor a ser transferido");
+                        
+                        if(contaOrigem != null && contaDestino != null)
+                            contaOrigem.transferir(contaDestino, Double.parseDouble(vTrans));
+                        
                         break;
                     case 4:
+                        String idSaldo;
+                        ContaCorrente conta=null;
+                        idSaldo = JOptionPane.showInputDialog("Informe a C/C");
+                        
+                        for(ContaCorrente cc: listaContas){
+                            if(cc.getId() == Integer.parseInt(idSaldo)){
+                                conta = cc;
+                                break;
+                            }
+                        }
+                        if (conta != null)
+                            JOptionPane.showMessageDialog(null, 
+                                    "NÚMERO DA CONTA: "+conta.getId()+
+                                    "SALDO: "+conta.getSaldo()
+                                    );
+                        else
+                            JOptionPane.showMessageDialog(null,"CONTA NÃO ENCONTRADA!");
+                        
                         break;
                     case 5:
+                        String idDeposito;
+                        String vDeposito;
+                        ContaCorrente contaDeposito = null;
+                        
+                        idDeposito = JOptionPane.showInputDialog( "Informe a C/C Origem");
+                        
+                        for(ContaCorrente cc: listaContas){
+                            if(cc.getId() == Integer.parseInt(idDeposito)){
+                                contaDeposito = cc;
+                                break;
+                            }
+                        }
+                        vDeposito = JOptionPane.showInputDialog("Informe a valor de depósito");
+                        
+                        contaDeposito.depositar(Double.parseDouble(vDeposito));
                         break;
                     case 6:
+                        String idSacado;
+                        String vSacado;
+                        ContaCorrente contaSacado = null;
+                        
+                        idSacado = JOptionPane.showInputDialog( "Informe a C/C Origem");
+                        
+                        for(ContaCorrente cc: listaContas){
+                            if(cc.getId() == Integer.parseInt(idSacado)){
+                                contaSacado = cc;
+                                break;
+                            }
+                        }
+                        vSacado = JOptionPane.showInputDialog("Informe a valor de depósito");
+                        
+                        if(contaSacado.sacar(Double.parseDouble(vSacado)))
+                            JOptionPane.showMessageDialog(null, "Sacado com sucesso...");
+                        else
+                            JOptionPane.showMessageDialog(null, "Falha ao efetuar o saque...");
+                        
+                        break;
+                    case 7:
+                        Cliente dependente;
+                        String opcCliente;
+                        String idCli, auxDep;
+                        
+                        idCli = JOptionPane.showInputDialog("Informe o ID da C/C");
+                        
+                        for(ContaCorrente cc: listaContas){
+                            if(cc.getId() == Integer.parseInt(idCli)){
+                                if(cc instanceof ContaCorrentePF){
+                                    dependente = new ClientePF();
+                                    auxDep = JOptionPane.showInputDialog("Nome do Cliente");
+                                    dependente.setNome(auxDep);
+                                    auxDep = JOptionPane.showInputDialog("ID do Cliente");
+                                    dependente.setId(Integer.parseInt(auxDep));
+                                    auxDep = JOptionPane.showInputDialog("CPF do Cliente");
+                                    ((ClientePF)dependente).setCpf(auxDep);
+                                    cc.setClienteDependente(dependente);
+                                }else{
+                                    JOptionPane.showMessageDialog(null, "Não é possível cadastrar dependente para pessoas jurídicas");
+                                }
+                            }
+                        }
+                        
                         break;
                     case 0:
                         System.exit(0);
