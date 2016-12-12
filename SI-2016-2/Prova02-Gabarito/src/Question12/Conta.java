f0480b0b9276389c8fdb27dbf45ea29c9474022d
Rafael Silva Guimarães <rafaelg@ifes.edu.br>
2016-12-12 19:07:45 -0200

    Gabarito Prova 02

---
+++ SI-2016-2/Prova02-Gabarito/src/Question12/Conta.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Question12;
+
+/**
+ *
+ * @author 20151si014
+ */
+public class Conta {
+    private int codAgencia;
+    private String nome;
+    private String numConta;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+    private String tipo;
+
+
+    public int getCodAgencia() {
+        return codAgencia;
+    }
+
+    public void setCodAgencia(int codAgencia) {
+        this.codAgencia = codAgencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(String numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
